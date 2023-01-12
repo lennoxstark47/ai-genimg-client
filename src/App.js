f18e944fae6f537dd2d2dc6acff4c38514396bf0
lennoxstark47 <twisamhaldar@gmail.com>
2023-01-12 21:28:42 +0530

    added func for cloud API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {
+	useEffect,
+	useState
+} from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	const [desc, setDesc] = useState('');
+	const [url, setUrl] = useState('');
+	const [loading, setLoading] = useState(false);
+	// useEffect(
+	// 	() => {
+	// 		console.log(desc);
+	// 	},
+	// 	[desc]
+	// );
+
+	const onClickHandler = () => {
+		const newPrompt = {
+			prompt: desc
+		};
+		setLoading(true);
+		axios
+			.post(
+				'https://twisam-aigenimg-api.onrender.com/openai/genimg',
+				newPrompt
+			)
+			.then(response => {
+				if (response) {
+					setLoading(false);
+				}
+				console.log(response);
+				setUrl(response.data.data);
+			})
+			.catch(err => {
+				console.log(err);
+			});
+	};
+	return (
+		<div
+			style={{
+				display: 'flex',
+				flexDirection: 'column',
+				alignItems: 'center'
+			}}>
+			<div
+				class="input-group mb-3"
+				style={{ width: 500 }}>
+				<input
+					type="text"
+					class="form-control"
+					placeholder="Enter description"
+					aria-label="Username"
+					aria-describedby="basic-addon1"
+					onChange={e => setDesc(e.target.value)}
+					value={desc}
+				/>
+				<button
+					type="button"
+					class="btn btn-primary"
+					onClick={e => onClickHandler()}>
+					Submit
+				</button>
+			</div>
+			<div>
+				{loading && <p>loading...</p>}
+				{url &&
+					!loading &&
+					<img
+						src={url}
+						alt="Ai"
+						style={{
+							width: '500px',
+							height: '500px'
+						}}
+					/>}
+			</div>
+		</div>
+	);
 }
-
-export default App;
